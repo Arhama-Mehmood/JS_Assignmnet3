678ba344b4359b242fe7f4e903dd85abf1adae47
Arhama Mehmood <arhamamehmood193@gmail.com>
2025-02-20 15:16:58 -0800

    ...

---
+++ app.js
@@ -0,0 +1,70 @@
+// Chapter 9-11 [PDF]
+
+// Q1)
+var cityName= prompt("Enter your city name: ");
+if(cityName="Karachi"){
+    alert("Welcome to city of lights.");
+}
+
+// Q2)
+var gender= prompt("Enter your gender: ");
+if(cityName="Male"){
+    alert("Good Morning Sir.");
+}
+else{
+    alert("Good Morning Ma'am.");
+}
+
+// Q3)
+
+// Q4)
+var remainingFuel= prompt("Enter your remaining fuel in litres: ");
+if(remainingFuel<0.25){
+    alert("Please refill the fuel in your car");
+}
+
+// Q5)
+
+// a
+var a = 4;
+if (++a === 5){
+alert("Given condition for variable a is true");
+}
+
+// b
+var b = 82;
+if (b++ === 83){
+alert("given condition for variable b is true");}
+
+// c
+var c = 12;
+if (c++ === 13){
+    alert("condition 1 is true");
+    }
+    if (c === 13){
+    alert("condition 2 is true");
+    }
+    if (++c < 14){
+    alert("condition 3 is true");
+    }
+    if(c === 14){
+    alert("condition 4 is true");}
+
+// d
+var materialCost = 20000;
+var laborCost = 2000;
+var totalCost = materialCost + laborCost;
+if (totalCost === laborCost + materialCost){
+alert("The cost equals");}
+
+// e
+if (true){
+    alert("True");
+    }
+    if (false){
+    alert("False");}
+
+// f
+if("car" < "cat"){
+    alert("Car is smaller than cat");}
+
