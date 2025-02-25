6c43fb32319d2d22ed1500e81cf84605ca8398f7
Arhama Mehmood <arhamamehmood193@gmail.com>
2025-02-24 17:56:54 -0800

    ...

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ else{
 }
 
 // Q3)
+var trafficColor=prompt("Enter color of road traffic signal:");
 
 // Q4)
 var remainingFuel= prompt("Enter your remaining fuel in litres: ");
@@ -68,3 +69,98 @@ if (true){
 if("car" < "cat"){
     alert("Car is smaller than cat");}
 
+// Q6)
+var maths,english,physics,obtMarks,percentage,grade,remarks;
+maths=+prompt("Please enter maths marks:");
+english=+prompt("Please enter english marks:");
+physics=+prompt("Please enter physics marks:");
+obtMarks=maths+english+physics;
+percentage=(obtMarks/300)*100;
+document.write("<h1>Marks Sheet<br></h1>");
+document.write("Total Marks: 300<br>");
+document.write("Marks Obtained: ",obtMarks);
+document.write("<br>Percentage: ",percentage,"%");
+
+if(percentage>=80){
+    document.write("<br>Grade = A-one");
+    document.write("<br>Remarks: Excellent");}
+  else if(percentage>=70){
+    document.write("<br>Grade = A");
+    document.write("<br>Remarks: Good");}
+  else if(percentage>=60){
+    document.write("<br>Grade = B");
+    document.write("<br>Remarks: You need to improve.");
+  }  
+else{
+    document.write("<br>Grade = Fail");
+    document.write("<br>Remarks: Sorry");
+}
+
+// Q7)Guess Game:
+var secretNumber= 6;
+guessNum=+prompt("Enter our secret number: ");
+if(guessNum==secretNumber){
+alert("Bingo! Correct Answer");
+}
+else if((guessNum+1)==secretNumber){
+    alert("Close enough to correct answer.");
+}
+
+// Q8)
+var num=+prompt("Enter any number: ");
+if(num%3==0){
+    alert("Yeahh! Your number is completely divisible by 3.");
+}
+
+// Q9)
+var num=+prompt("Enter any number to check whether it is even or odd: ");
+if(num%2==0){
+    alert("The inputted number is even.");
+}
+else{
+    alert("The inputted number is odd.");
+}
+
+// Q10)
+var temp=+prompt("Please Enter Current temperature:");
+if(temp>40){
+    alert("It is too hot outside");
+}
+else if(temp>30){
+    alert("The weather today is Normal.");
+}
+else if(temp>20){
+    alert("Today's weather is Cool.");
+}
+else if(temp>10){
+    alert("OMG! Today's weather is so Cool");
+}
+
+// Q11)
+var firstNum=+prompt("Enter First Number:");
+var secondNum=+prompt("Enter Second Number:");
+var operattion=prompt("Enter the opertor which you want to apply on numbers:");
+if(operattion=="+"){
+    var add=firstNum+secondNum;
+    document.write("The addition of both numbers are: ",add);}   
+ else if(operattion=="-"){
+    var subtract=firstNum-secondNum;
+    document.write("The Subtraction of both numbers are: ",subtract);}
+else if(operattion=="/"){
+    var divide=firstNum/secondNum;
+    document.write("The Division of both numbers are: ",divide);}
+else if(operattion=="%"){
+    var modulus=firstNum%secondNum;
+    document.write("The Modulus of both numbers are: ",modulus);}
+else if(operattion=="*"){
+    var multilply=firstNum*secondNum;
+    document.write("The Multiplication of both numbers are: ",multilply);}
+else{
+    alert("Invalid Operator!");
+}
+
+// X============================================================================================================================X
+
+// Chapter 12-13 [PDF]
+
+// Q1)
